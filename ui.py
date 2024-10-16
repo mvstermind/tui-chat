fe6df3c72b010e3276be95199c41ade34d84daed
mvstermind <zablockidawid1@gmail.com>
2024-10-17 00:25:44 +0200

    goin to sleep

--- ui.py
+++ ui.py
@@ -1,16 +1,9 @@
+import enum
 import os
 import tkinter
 from pynput.mouse import Button, Controller
+import errors
 
-
-class TextOverlap(Exception):
-    """Exception raised when text overlaps other text"""
-    def __init__(self, message: str, error_code:int):
-        super().__init__(message)
-        self.error_code = error_code
-        self.message = message
-    def __str__(self):
-        return f"{self.message} (Error Code: {self.error_code})" 
 
 class Display:
     def __init__(self) -> None:
@@ -30,7 +23,7 @@ class Display:
         self.mouse_y: int = self.__get_mouse_y()
 
         # this contains text that is displayed and x, y cords
-        self.data_stored: dict{str: tuple(int,int)} 
+        self.data_stored: dict[str, tuple[int, int]] = {}
 
     def __get_mouse_x(self) -> int:
         mouse = Controller()
@@ -46,22 +39,32 @@ class Display:
     def clear_screen(self) -> None:
         print("\033[2J\033[H")
 
+    def text_on_screen(self, text: str, x_cords: int, y_cords: int) -> None:
+        if x_cords + len(text) > self.TERMINAL_COLUMNS:
+            raise errors.OutOfBounds(
+                f"Text {text} will be out of bounds for x: {x_cords}", 3
+            )
+        if y_cords > self.TERMINAL_ROWS:
+            raise errors.OutOfBounds(
+                f"Text {text} will be out of bounds for y: {y_cords}", 4
+            )
 
-    def text_on_screen(self,text:str, x:int, y:int)-> None:
+        if len(self.data_stored) != 0:
+            for key, value in enumerate(self.data_stored):
+                if y_cords == value[0] and x_cords == value[1]:
+                    raise errors.TextOverlap(f'Text "{text}" overlaps {key}', 2)
 
-        for data in self.data_stored.values():
-            if y == data[0] and x == data[1]:
-                raise TextOverlap(f"Text \"{text}\" overlaps {self.data_stored[data]}", 2)
+        self.data_stored[text] = (x_cords, y_cords)
+        names = []
+        for names in sorted(self.data_stored.keys()):
+            
 
 
 def main():
     display = Display()
-    display.clear_screen()
-    # while True:
-    #     try:
-    #         print(display.get_mouse_position())
-    #     except KeyboardInterrupt:
-    #         print("done")
+    display.text_on_screen(text="first", x_cords=1, y_cords=10)
+    display.text_on_screen(text="third", x_cords=4, y_cords=13)
+    display.text_on_screen(text="second", x_cords=2, y_cords=12)
 
 
 if __name__ == "__main__":
