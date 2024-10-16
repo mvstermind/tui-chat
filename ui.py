0090fc3ac168c690c54bf521e30883db2778fc03
mvstermind <zablockidawid1@gmail.com>
2024-10-16 22:54:42 +0200

    g

--- ui.py
+++ ui.py
@@ -9,13 +9,18 @@ class Display:
         tsize: os.terminal_size = os.get_terminal_size()
         self.TERMINAL_COLUMNS: int = tsize[0]
         self.TERMINAL_ROWS: int = tsize[1]
+
         self.SCREEN_WIDTH: int = self.root.winfo_screenwidth()
         self.SCREEN_HEIGHT: int = self.root.winfo_screenheight()
+
         self.TERMINAL_CELL_WIDTH: int = int(self.SCREEN_WIDTH / self.TERMINAL_COLUMNS)
         self.TERMINAL_CELL_HEIGHT: int = int(self.SCREEN_HEIGHT / self.TERMINAL_ROWS)
 
         self.mouse_x: int
         self.mouse_y: int
+
+        # this contains text that is displayed and x, y cords
+        self.text_stored: dict{str: tuple(int,int)} 
 
     def get_mouse_position(self) -> tuple[int, int]:
         mouse = Controller()
@@ -23,14 +28,23 @@ class Display:
         self.mouse_y = round(mouse.position[1])
         return self.mouse_x, self.mouse_y
 
+    def clear_screen(self) -> None:
+        print("\033[2J\033[H")
+
+
+    def text_on_screen(self,text:str, x:int, y:int)-> None:
+
+        for text in self.text_stored:
+
 
 def main():
     display = Display()
-    while True:
-        try:
-            print(display.get_mouse_position())
-        except KeyboardInterrupt:
-            print("done")
+    display.clear_screen()
+    # while True:
+    #     try:
+    #         print(display.get_mouse_position())
+    #     except KeyboardInterrupt:
+    #         print("done")
 
 
 if __name__ == "__main__":
